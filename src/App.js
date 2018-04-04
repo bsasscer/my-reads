8d334fe50b8a753cd8fba77298f93962e36cf025
bsasscer <smithbi.maricela@gmail.com>
2018-04-04 13:23:12 -0700

    use the getAll method in the BooksAPI to fetch all the starter books and map them

--- src/App.js
+++ src/App.js
@@ -2,22 +2,39 @@ import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import BookList from './BookList'
 import Search from './Search'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
 
+  state = {
+      books: []
   }
 
+
+      componentDidMount() {
+        BooksAPI.getAll()
+        .then((books) => {
+            this.setState(() => ({
+                books
+            }))
+        })
+    }
+
+
   render() {
+      console.log(this.state)
     return (
         <div className="app">
             <div className="list-books">
                 <div className="list-books-title">
                   <h1>MyReads</h1>
                 </div>
-                <Route exact path="/" component={BookList} />
+                <Route exact path="/" render={() => (
+                    <BookList
+                        books={this.state.books}
+                    />
+                )} />
                 <Route path="/search" component={Search} />
                 <div className="open-search">
                     <Link to="/search">Add a book</Link>
--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 
 class Search extends Component {
 
+    state = {
+        books: [],
+        query: ''
+    }
+
+    
+
+    updateQuery = (query) => {
+        this.setState(() => ({
+            query: query.trim()
+        }))
+    }
+
+    clearQuery = () => {
+        this.updateQuery('')
+    }
+
     render() {
+        const { query } = this.state
+        const { books } = this.props
+
+
+        console.log(query)
+
         return (
             <div className="search-books">
                 <div className="search-books-bar">
@@ -20,11 +44,25 @@ class Search extends Component {
                         However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                         you don't find a specific author or title. Every search is limited by search terms.
                       */}
-                      <input type="text" placeholder="Search by title or author"/>
+                      <input
+                          type="text"
+                          onChange={(event) => this.updateQuery(event.target.value)}
+                          placeholder="Search by title or author"
+                          value={query}
+                      />
                     </div>
                 </div>
+                <button onClick={this.clearQuery}>Clear</button>
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <ol className="books-grid">
+                        {/* {books.map((book) => (
+                        <li key={book.id}>
+                            <div>
+                                <p>{book.title}</p>
+                            </div>
+                        </li>
+                        ))} */}
+                    </ol>
                 </div>
             </div>
         )
